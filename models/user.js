563f790731f96653681c00efe83081ac54072a9f
GlennChon <glenn.chon@gmail.com>
2019-08-26 16:26:58 +0700

    Added auth and user route, updated user model

--- models/user.js
+++ models/user.js
@@ -1,18 +1,53 @@
 const mongoose = require("mongoose");
+const Joi = require("joi"); // Input Validation
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  firstname: String,
-  lastname: String,
-  email: String,
-  recipesAdded: Number,
-  likes: [
-    {
-      recipe: mongoose.ObjectId
-    }
-  ]
+  username: { type: String, unique: true, required: true },
+  firstname: { type: String, default: "", maxlength: 50 },
+  lastname: { type: String, default: "", maxLength: 50 },
+  about: { type: String, default: "" },
+  email: { type: String, unique: true, required: true },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 1024
+  },
+  isAdmin: { type: Boolean, default: false },
+  recipes: [{}],
+  totalLikes: { type: Number, default: 0, required: true }
 });
+
+userSchema.methods.generateAuthToken = function() {
+  return jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+};
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+// Joi validation
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(1024)
+      .required()
+  };
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const Joi = require("joi");
+const { User } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
+
+// Register User
+router.post("/", async (req, res) => {
+  // Validation schema using Joi
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  //user exists
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  //password matches
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  const token = user.generateAuthToken();
+  res.send(token);
+});
+
+// Joi validation
+function validate(req) {
+  const schema = {
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(1024)
+      .required()
+  };
+  return Joi.validate(req, schema);
+}
+
+module.exports = router;
