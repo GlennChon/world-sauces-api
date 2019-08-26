27bac2953aa341432e6630391fc74f577c4a4c67
GlennChon <glenn.chon@gmail.com>
2019-08-27 00:47:23 +0700

    removed unnecessary recipe schema and object req

--- routes/countries.js
+++ routes/countries.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { Country, validateCountry } = require("../models/country");
-const { Recipe, validateRecipe } = require("../models/recipe");
 // COUNTRIES
 // Get list of countries
 router.get("/", async (req, res) => {
