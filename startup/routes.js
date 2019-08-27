16c33d13ce75464ba29f967468ff9b440dd0fb00
GlennChon <glenn.chon@gmail.com>
2019-08-27 21:51:25 +0700

    reformat

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,12 @@
+const helmet = require("helmet");
 const express = require("express");
-const homeRoute = require("./routes/home");
-//const usersRoute = require("./routes/users");
-const recipesRoute = require("./routes/recipes");
-const countriesRoute = require("./routes/countries");
-const tasteProfilesRoute = require("./routes/tasteProfiles");
 const error = require("../middleware/error");
+const auth = require("../middleware/auth");
+const docsRoute = require("../routes/docs");
+//const usersRoute = require("../routes/users");
+const recipesRoute = require("../routes/recipes");
+const countriesRoute = require("../routes/countries");
+const tasteProfilesRoute = require("../routes/tasteProfiles");
 
 module.exports = function(app) {
   app.use(express.json());
@@ -12,7 +14,7 @@ module.exports = function(app) {
   app.use(express.urlencoded({ extended: true }));
   app.use(helmet());
 
-  app.use("/", homeRoute);
+  app.use("/api/docs", docsRoute);
   app.use("/api/auth", auth);
   //app.use("/api/users", usersRoute);
   app.use("/api/recipes", recipesRoute);
