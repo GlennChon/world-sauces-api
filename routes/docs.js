e9d90bcb8afdccfdbf0ec092760e439564c13ada
GlennChon <glenn.chon@gmail.com>
2019-08-27 21:46:56 +0700

    changed home to docs route

--- routes/docs.js
+++ routes/docs.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
+
 // Home
-// Get
 router.get("/", (req, res) => {
-  res.render("index", { title: "World Sauce Api Home Page", message: "Hello" });
+  res.render("body", {
+    title: "World Sauce Api Documentation Page",
+    message: "Hello"
+  });
 });
 
 module.exports = router;
