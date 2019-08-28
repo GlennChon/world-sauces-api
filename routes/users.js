d7082cec4d3a7203dd100a4aee215d68aa2261c4
GlennChon <glenn.chon@gmail.com>
2019-08-28 16:24:38 +0700

    working - get  by username and post new

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,34 @@
 const auth = require("../middleware/auth");
-const { User, validate } = require("../models/user");
+const { User, validateUser } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 
-router.get("/me", auth, async (req, res) => {
-  const user = await User.findById(req.user._id).select("-password"); // get user object, exclude password
+router.get("/:username", auth, async (req, res) => {
+  const user = await User.findOne({
+    username: new RegExp("^" + req.params.username + "$", "i")
+  }).select("-password -email");
+  if (!user) return res.status(400).send("Invalid username.");
   res.send(user);
 });
 
 // Register User
 router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
+  const { error } = validateUser(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  user = new User(_.pick(req.body, ["username", "email", "password"]));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
-  await user.save();
+  try {
+    await user.save();
+  } catch (err) {
+    console.log(err);
+  }
 
   const token = user.generateAuthToken();
   res
