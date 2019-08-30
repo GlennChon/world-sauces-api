7c81a5283b1330258153f571d5963f25b3542ac2
GlennChon <glenn.chon@gmail.com>
2019-08-30 20:39:02 +0700

    added check for user and email for logins

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,29 @@
 const Joi = require("joi");
-const { User } = require("../models/user");
-const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
+const express = require("express");
+const { User } = require("../models/user");
 
-// Register User
+const router = express.Router();
+
+// Authenticate User
 router.post("/", async (req, res) => {
   // Validation schema using Joi
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  //user exists
-  let user = await User.findOne({ email: req.body.email });
+  // Check if user exists
+  // By email
+  let user = await User.findOne({ email: req.body.user });
+  // By username
+  if (!user) {
+    const usernameRegex = new RegExp("^" + req.body.user + "$", "i");
+    user = await User.findOne({ username: usernameRegex });
+  }
+  // No result
   if (!user) return res.status(400).send("Invalid email or password");
 
-  //password matches
+  // Check if password matches
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send("Invalid email or password");
 
@@ -27,11 +34,10 @@ router.post("/", async (req, res) => {
 // Joi validation
 function validate(req) {
   const schema = {
-    email: Joi.string()
+    user: Joi.string()
       .min(5)
       .max(255)
-      .required()
-      .email(),
+      .required(),
     password: Joi.string()
       .min(5)
       .max(1024)
