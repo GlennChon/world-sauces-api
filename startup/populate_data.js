794d05834cb49f7c244caf128ffb9044775af9c8
GlennChon <glenn.chon@gmail.com>
2019-09-15 16:22:09 +0700

    added async decorator

--- startup/populate_data.js
+++ startup/populate_data.js
@@ -1,37 +1,32 @@
+const assert = require("assert");
 
-const assert = require('assert');
-
-const countries = require('../data/countries.json');
-const tasteProfiles = require('../data/tasteProfiles.json');
-const recipes = require('../data/recipes.json');
+const countries = require("../data/countries.json");
+const tasteProfiles = require("../data/tasteProfiles.json");
+const recipes = require("../data/recipes.json");
 const { Country } = require("../models/country");
 const { TasteProfile } = require("../models/tasteProfile");
 const { Recipe } = require("../models/recipe");
 
-module.exports = function populate_data(){
+module.exports = async function populate_data() {
+  const countryCount = await Country.count();
+  if (countryCount < 1) {
+    importData(Country, countries);
+  }
 
-    const countryCount = await Country.count();
-    if (countryCount < 1){
-       importData(Country, countries);
-    }
+  const tprofileCount = await TasteProfile.count();
+  if (tprofileCount < 1) {
+    importData(TasteProfile, tasteProfiles);
+  }
 
-    const tprofileCount = await TasteProfile.count();
-    if (tprofileCount < 1){
-        importData(TasteProfile, tasteProfiles);
-    }
+  const recipeCount = await Recipe.count();
+  if (recipeCount < 1) {
+    importData(Recipe, recipes);
+  }
+};
 
-    
-    const recipeCount = await Recipe.count();
-    if (recipeCount < 1){
-        importData(Recipe, recipes);
-    }
-
+async function importData(model, data) {
+  await model.collection.insertMany(data, function(err, r) {
+    assert.equal(null, err);
+    assert.equal(3, r.insertedCount);
+  });
 }
-
-function importData(model, data){
-    model.collection.insertMany(data, function(err,r) {
-        assert.equal(null, err);
-          assert.equal(3, r.insertedCount);
-        })
-}
-
