df700c00159edb94cda8074e390af887cf3272cb
GlennChon <glenn.chon@gmail.com>
2019-09-16 15:02:06 +0700

    added Recipe model require

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const express = require("express");
 const auth = require("../middleware/auth");
 const admin = require("../middleware/admin");
 const { User, validateUser, validateUserUpdate } = require("../models/user");
+const { Recipe } = require("../models/recipe");
 
 const mongoose = require("mongoose");
 const Fawn = require("fawn");
